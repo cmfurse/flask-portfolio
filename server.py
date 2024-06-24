7d76ebc6ae45552a67ed0c79f6fc682fbd8217a5
Clint Furse <cmfurse@gmail.com>
2024-06-24 11:04:41 -0600

    added templates

--- server.py
+++ server.py
@@ -1,11 +1,33 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def home():
+    return render_template('index.html')
+
+
+@app.route('/<string:page_name>')
+def html_page(page_name):
+    return render_template(page_name)
+
+
+def write_to_file(data):
+    with open('database.txt', mode='a') as database:
+        email = data['email']
+        subject = data['subject']
+        message = data['message']
+        file = database.write(f'\n{email},{subject},{message}')
+
+@app.route('/submit_form', methods=['POST', 'GET'])
+def submit_form():
+    if request.method == 'POST':
+        data = request.form.to_dict()
+        write_to_file(data)
+    else:
+        return 'something went wrong.  Try again.'
+    return redirect('/thankyou.html')
 
 
 if __name__ == '__main__':
